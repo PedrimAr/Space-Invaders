78e2ded2b483c18d4f6920b562dc2dd505024133
Pedro do Amaral Rangel <pedro_ar@id.uff.br>
2025-01-10 12:13:20 -0300

    Add files via upload

--- enemies.py
+++ enemies.py
@@ -5,10 +5,18 @@ game_speed = 400
 
 
 def spawn_enemy(i, j, matriz_enemies):
+    boss_x = int(random.uniform(0, i - 1))
+    boss_y = int(random.uniform(0, j - 1))
     for x in range(i):
         for y in range(j):
-            enemy = Sprite("png/nave_inimiga.png", 1)
-            enemy.set_position(x * (enemy.width + 10) + 200, y * (enemy.height + 10) + 140)
+            if x == boss_x and y == boss_y:
+                enemy = Sprite("png/nave_boss.png", 1)
+                enemy.life = 2
+            else:
+                enemy = Sprite("png/nave_inimiga.png", 1)
+                enemy.life = 1
+
+            enemy.set_position(x * (enemy.width + 10) + 50, y * (enemy.height + 10) + 10)
             # Define a direção do movimento, no caso para baixo
             enemy.direction = 1
             # Define randomicamente o intervalo entre os disparos
